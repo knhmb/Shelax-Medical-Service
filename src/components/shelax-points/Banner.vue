a1f7bf1bc2fc448f3d89ebf636861e2ec3f0911f
knhmb <khaled@marvelconnect.com>
2022-04-21 12:18:50 +0800

    Modifications

--- src/components/shelax-points/Banner.vue
+++ src/components/shelax-points/Banner.vue
@@ -3,13 +3,30 @@
     <base-content-container>
       <h3>Shelax Points 介紹</h3>
       <p>你發位王珠借立乍村文遠前把石象</p>
+      <el-row justify="space-between">
+        <el-col :sm="10" :md="5">
+          <img src="../../assets/visual-sample1.png" alt="" />
+          <p>1. 成為會員</p>
+          <p>吃申三旦力未歡們了同半干十跳而菜用未歡</p>
+        </el-col>
+        <el-col :sm="10" :md="5">
+          <img src="../../assets/visual-sample1.png" alt="" />
+          <p>2. 累積點數</p>
+          <p>吃申三旦力未歡們了同半干十跳而菜用未歡</p>
+        </el-col>
+        <el-col :sm="10" :md="5">
+          <img src="../../assets/visual-sample1.png" alt="" />
+          <p>3. 好禮兌換</p>
+          <p>吃申三旦力未歡們了同半干十跳而菜用未歡</p>
+        </el-col>
+      </el-row>
     </base-content-container>
   </section>
 </template>
 
 <style scoped>
 .shelax-banner {
-  padding: 3rem 0;
+  padding: 3rem 0 5rem 0;
   background: #8d9de8;
 }
 
@@ -31,6 +48,35 @@
   line-height: 120%;
   color: #ffffff;
   text-align: center;
-  margin: 1rem 0;
+  margin: 1rem 0 6rem 0;
+}
+
+.shelax-banner img {
+  width: 100%;
+}
+
+.shelax-banner .el-row .el-col p:first-of-type {
+  font-family: "Noto Sans HK";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  letter-spacing: 0.1px;
+  color: #ffffff;
+  margin: 1rem 0 0.5rem 0;
+}
+
+.shelax-banner .el-row .el-col p:nth-of-type(2) {
+  font-family: "Noto Sans HK";
+  font-style: normal;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 20px;
+  text-align: center;
+  letter-spacing: 0.1px;
+  color: #26408a;
+  margin: 0;
+  margin-bottom: 2.5rem;
 }
 </style>
